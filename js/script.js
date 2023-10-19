c640e2370315087f5e241d61c2c2c3f55246f81a
Nikita <rutina0grib@gmail.com>
2023-10-19 10:56:35 +0300

    added '...respons' in variable, a DIV with a class is created 'menu__item'

--- js/script.js
+++ js/script.js
@@ -156,12 +156,13 @@ window.addEventListener('DOMContentLoaded', () => {
   // Используем классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -173,16 +174,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       const element = document.createElement('div');
+
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-        <div class="menu__item">
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
       `;
       this.parent.append(element);
@@ -205,7 +212,7 @@ window.addEventListener('DOMContentLoaded', () => {
     "img/tabs/elite.jpg",
     "elite",
     'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, ни качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, ни качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     14,
     '.menu .container'
   ).render();
